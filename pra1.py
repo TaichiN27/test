6fbdbda7c4fbe630aa4c83a475dc44997ede52b3
Taichi Nakamura <zhongcuntaide@Taichis-MacBook-Air.local>
2023-04-10 18:39:58 +0900

    first

---
+++ pra1.py
@@ -0,0 +1,21 @@
+a = 100
+b = a 
+c = b
+print(type(b))
+
+
+string = "Hi! My name is Mike"
+
+is_start = string.startswith("Hi!")
+
+print(is_start)
+
+
+list = ['a', 'b', 'c']
+
+print(list[0])
+
+tuple = (10, 10, 30)
+
+print(tuple[0])
+
---
+++ pra2.py
@@ -0,0 +1,66 @@
+import random
+
+def in_order(num):
+    for i in range(len(num)-1):
+        if num[i]>num[i+1]:
+            return False
+    return True
+
+def bogo_sort(num):
+    while not in_order(num):
+        random.shuffle(num)
+    return num
+
+
+if __name__ == '__main__':
+    print(bogo_sort([1,3,2,4]))
+
+
+#dictionary
+d = {"item1": "car", "item2": "bike"}
+print("item2" in d)
+
+set = {1,2,2,23,4,5}
+
+set.add(0)
+print(set)
+
+
+
+
+array = ['apple', 'banana', 'grape']
+array2 = ["good", "yea", "amazing"]
+
+for i, a in enumerate (array):
+    print(i)
+
+for a, b in zip(array, array2):
+    print(a+b)
+
+
+dic= {"x":"hey", "y":"oh"}
+
+for k, v in dic.items():
+    print(k + v)
+
+
+def start(a, b):
+    print(a + " " + "hey")
+
+
+start("Oh", "whatever")
+
+def roopKV(**kwargs):
+    for k, v in kwargs.items():
+        print(k + v)
+
+d = {
+    '1':'hey',
+    '2':'okay',
+    '3':'oh'
+    }
+
+
+roopKV(**d)
+
+print("hey")
